43849bf0c5324126c6eeca3ad25af4eae0f7da89
Rohan27s <74227860+Rohan27s@users.noreply.github.com>
2021-06-14 20:57:55 +0530

    Create Majority Element using bit manipulation.cpp

---
+++ Majority Element using bit manipulation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = 0;
+        for (unsigned int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
+            int bits = 0;
+            for (int num : nums) {
+                if (num & mask) {
+                    bits++;
+                }
+            }
+            if (bits > nums.size() / 2) {
+                majority |= mask;
+            }
+        }
+        return majority;
+    }
+};

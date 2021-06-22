a61614fa165713385a092c9740ff41f5bf4af21f
Rohan27s <74227860+Rohan27s@users.noreply.github.com>
2021-06-22 19:34:56 +0530

    Create Sum of Left Leaves.cpp

---
+++ Sum of Left Leaves.cpp
@@ -0,0 +1,9 @@
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root, bool isleft = false) {
+    if (!root) return 0;
+    if (!root->left && !root->right) return isleft ? root->val : 0;
+    return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false);
+}
+};
